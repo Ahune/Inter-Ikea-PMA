5882fbb80ec293aa0ae8b67b157dbce2e3de4c2a
Alexander <Alexandermounzer@gmail.com>
2025-03-10 10:21:58 +0100

    Add / create Xunit Test Project and prepare with Moq

---
+++ webapi/Tests/ProductManagementApp.Tests/ProductServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using webapi.Domain.Interfaces.Repositories;
+using webapi.Domain.Services;
+using Xunit;
+
+namespace ProductManagementApp.Tests;
+
+public class ProductServiceTests
+{
+    private readonly Mock<IProductRepository> _productRepositoryMock;
+    private readonly ProductService _productService;
+    
+    public ProductServiceTests()
+    {
+        _productRepositoryMock = new Mock<IProductRepository>();
+        _productService = new ProductService(_productRepositoryMock.Object);
+    }
+    
+}
